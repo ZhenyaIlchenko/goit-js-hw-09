b2f1fe4d9f9b4ea166c805e1942d2fbeb15aacf3
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-24 16:24:51 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId = null;
+
+const refs = {
+  body: document.querySelector('body'),
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+
+refs.buttonStart.addEventListener('click', handleClickStart);
+refs.buttonStop.addEventListener('click', handleClickStop);
+
+function checkControlAvailability() {
+  if (intervalId) {
+    refs.buttonStart.setAttribute('disabled', '');
+    refs.buttonStop.removeAttribute('disabled');
+  } else {
+    refs.buttonStart.removeAttribute('disabled');
+    refs.buttonStop.setAttribute('disabled', '');
+  }
+}
+
+checkControlAvailability();
+
+function handleClickStart() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+
+  if (!intervalId) {
+    intervalId = setInterval(handleClickStart, 1000);
+  }
+  checkControlAvailability();
+}
+
+function handleClickStop() {
+  clearInterval(intervalId);
+  intervalId = null;
+  checkControlAvailability();
+}
